f05188775d2d5ca81def0238ef8f8754537e9366
Mohamed El-Sharkawy <melsharkawy99@hotmail.com>
2022-04-20 21:55:36 +0200

    add  favorite-model-route-cart-model-route

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -1,21 +1,30 @@
 const mongoose = require("mongoose");
 
+
 const CartSchema = new mongoose.Schema(
+
   {
-    userId: { type: String, required: true },
-    products: [
-      {
-        productId: {
-          type: String,
-        },
-        quantity: {
-          type: Number,
-          default: 1,
-        },
-      },
-    ],
-  },
-  { timestamps: true }
+      userId : {type: String, required:true},
+      products : [  // we can specify that the type is Array but we want to add some properties
+          {
+             productID: {
+                  type:String
+             },
+             quntity: {
+                 type:Number,
+                 default:1
+             },
+        
+
+          }
+
+      ],
+      totalPrice:{type:Number, default:0},
+    
+   
+      //createdAt : Date.now()
+  },{timestamps: true}// mongoose function : when it is created
+
 );
 
-module.exports = mongoose.model("Cart", CartSchema);
+module.exports = mongoose.model("CartSchema", CartSchema);
